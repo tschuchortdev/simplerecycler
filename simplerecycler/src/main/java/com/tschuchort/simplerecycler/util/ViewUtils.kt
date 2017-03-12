7d77d2eb9b1125345fa702db1cc269a1d74298aa
tschuchortdev <t.schuchort@googlemail.com>
2017-03-12 22:36:43 +0100

    rename packages

--- simplerecycler/src/main/java/com/tschuchort/simplerecycler/util/ViewUtils.kt
+++ simplerecycler/src/main/java/com/tschuchort/simplerecycler/util/ViewUtils.kt
@@ -1,11 +1,7 @@
-package com.tschuchort.endlessrecycler
+package com.tschuchort.simplerecycler
 
 import android.view.View
 import android.view.ViewGroup
-
-/**
- * Created by Thilo on 12/19/2016.
- */
 
 internal val ViewGroup.children: List<View>
 	get() = (0..childCount - 1).map { getChildAt(it) }
